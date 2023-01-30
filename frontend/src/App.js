29e373bfdbbd9e67ebc16a484b8aaad1862d0745
Rakeshsah0 <78860663+Rakeshsah0@users.noreply.github.com>
2023-01-30 23:53:34 +0545

    Student Management System project initialization

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import {Navbar,About,Feedback} from './components';
+
+function App() {
+  return (
+    <div className="homepage">
+    <Navbar/>
+    <About/>
+    <Feedback/>
+    </div>
+  );
+}
+
+export default App;
