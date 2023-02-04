e25a22b9624be132472b7cbfad2ff4120af3151b
Rajaram Yadav <yrajaram112@gmail.com>
2023-02-04 22:36:15 +0530

    Navbar Version 1 setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,31 @@
-import {Navbar,About,Feedback} from './components';
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import Gallery from './pages/gallery';
+import Academics from './pages/academics';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
 
 function App() {
-  return (
-    <div className="homepage">
-    <Navbar/>
-    <About/>
-    <Feedback/>
-    </div>
-  );
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		<Route path='/' exact component={Home} />
+		<Route path='/about' component={About} />
+    <Route path='/academics' component={Academics} />
+		<Route path='/events' component={Events} />
+		<Route path='/gallery' component={Gallery} />
+		
+		<Route path='/contact' component={Contact} />
+		<Route path='/sign-up' component={SignUp} />
+	</Routes>
+	</Router>
+);
 }
 
 export default App;
--- frontend/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export {default as Navbar} from './Navbar';
-export {default as About} from './About';
-export {default as Feedback} from './Feedback';
