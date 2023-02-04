9216834b4716ed6cffa623a82c462277054d944d
Rakeshsah0 <78860663+Rakeshsah0@users.noreply.github.com>
2023-02-04 22:44:19 +0545

    database connection eastablished and fetched data to check connectivity

--- backend/controllers/students.js
+++ backend/controllers/students.js
@@ -1,11 +1,27 @@
+import mysql from "mysql"
 
+const db=mysql.createPool({
+    host:"sql12.freesqldatabase.com",
+    user:"sql12595754",
+    password:"uMP5JXuRjC",
+    database:"sql12595754"
+})
 
 export const fetchStudents=(req,res)=>{
-    res.send("Hello from students fetch all")
+    const fetchSTudents="select * from Students;"
+    db.query(fetchSTudents,(error,result)=>{
+        if(error) console.log(error)
+        else res.send(result)
+    })
 }
 
 export const fetchStudent=(req,res)=>{
-    res.send(`Hello from student with id ${req.params.id}`)
+    const id=req.params.id
+    const fetchStudent=`select * from Students where student_id=${id}`
+    db.query(fetchStudent,(error,result)=>{
+        if(error) console.log(error)
+        else res.send(result)
+    })
 }
 
 export const createStudent=(req,res)=>{
