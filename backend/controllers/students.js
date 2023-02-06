0b129e7d89bf4e6add1f52c719bd803f96c021f6
Rakeshsah0 <78860663+Rakeshsah0@users.noreply.github.com>
2023-02-06 11:06:53 +0545

    sql server changed to lost host

--- backend/controllers/students.js
+++ backend/controllers/students.js
@@ -1,14 +1,14 @@
-import mysql from "mysql"
+import mysql from "mysql2"
 
 const db=mysql.createPool({
-    host:"sql12.freesqldatabase.com",
-    user:"sql12595754",
-    password:"uMP5JXuRjC",
-    database:"sql12595754"
+    host:"localhost",
+    user:"root",
+    password:"Xenon@6590",
+    database:"school"
 })
 
 export const fetchStudents=(req,res)=>{
-    const fetchSTudents="select * from Students;"
+    const fetchSTudents="select * from students;"
     db.query(fetchSTudents,(error,result)=>{
         if(error) console.log(error)
         else res.send(result)
