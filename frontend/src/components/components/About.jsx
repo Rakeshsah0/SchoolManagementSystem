fbdac133f434f0d481a5c5920daf114d24d31181
Rakeshsah0 <78860663+Rakeshsah0@users.noreply.github.com>
2023-02-19 00:08:20 +0545

    Intialized layouts , created water bubble template

--- frontend/src/components/components/About.jsx
+++ frontend/src/components/components/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import {WaterBubble} from '../layouts'
+import '../css/About.css'
 const About = () => {
+  const ourmission=[{"id":1,"title":"Quality Education"},{"id":2,"title":"Quality Education"},{"id":3,"title":"Quality Education"}]
   return (
-    <div>
-      
-      About</div>
+    <div className='wrapper'>
+      {ourmission.map(mission=><WaterBubble key={"our-mission-"+mission.id} number={mission.id} mission_name={mission.title}/>)}
+    </div>
   )
 }
 
