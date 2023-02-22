b8d966103838ca9f46a82e60382caf638083f705
Rakeshsah0 <78860663+Rakeshsah0@users.noreply.github.com>
2023-02-22 22:23:48 +0545

    updated teacher and student api

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,9 +1,8 @@
 import express from "express";
-import {fetchStudents,fetchStudent,createStudent} from "../controllers/students.js";
+import {fetchStudents,fetchStudent} from "../controllers/students.js";
 const router=express.Router()
 
 router.get('/',fetchStudents)
 router.get('/:student_id',fetchStudent)
-router.post('/createStudent',createStudent)
 
 export default router
--- backend/routes/teachers.js
+++ backend/routes/teachers.js
@@ -1,9 +1,9 @@
 import express from "express";
-import {fetchTeachers,fetchTeacher,createTeacher} from "../controllers/teachers.js";
+import {fetchTeachers,fetchTeacher} from "../controllers/teachers.js";
 const router=express.Router()
 
 router.get('/',fetchTeachers)
 router.get('/:teacher_id',fetchTeacher)
-router.post('/createStudent',createTeacher)
+
 
 export default router
