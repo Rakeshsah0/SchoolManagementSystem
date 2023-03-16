515ff2f1f9229d237d21b44d340bd9eb84d09866
Rakeshsah0 <78860663+Rakeshsah0@users.noreply.github.com>
2023-03-16 23:46:08 +0545

    login page change

--- frontend/src/components/components/Login.jsx
+++ frontend/src/components/components/Login.jsx
@@ -12,8 +12,7 @@ const Login = (setnavandfooter) => {
     <div>
       <Link to='/'><button onClick={()=>{setnavandfooter.setnavandfooter(true)}}>Homepage</button></Link>
       <h1>{profile}</h1>
-      Lorem ipsum dolor sit amet consectetur minima, exercitationem alias assumenda porro ab quo! At qui ut aliquam impedit voluptatum veritatis iusto architecto praesentium animi, numquam eligendi dolorum in sequi debitis reprehenderit non doloribus, quidem fugit. Possimus deserunt dolore, consequuntur iusto totam cupiditate sapiente veniam dolores consequatur aspernatur rem repellat voluptatem molestiae quam ea, ex rerum eius sed, doloribus nesciunt aut quidem accusantium molestias similique. Quae iste, ratione dolore repudiandae alias asperiores. Quam accusantium eveniet voluptate, enim optio laborum. Facere dolores consectetur rem, perspiciatis, earum iste consequuntur ut natus nemo molestias provident, quo reprehenderit fuga.
-      
+      Lorem ipsum dolor 
     </div>
   )
 }
