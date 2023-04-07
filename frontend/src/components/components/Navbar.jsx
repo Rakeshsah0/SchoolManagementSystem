e3dbebe65d3f67c04fca450048a1eac0388eb8c0
Rajaram Yadav <yrajaram112@gmail.com>
2023-04-07 10:58:41 +0545

    working

--- frontend/src/components/components/Navbar.jsx
+++ frontend/src/components/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React,{useState} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../css/Navbar.css'
-import {FaCalendar,FaUsers,FaPhoneAlt,FaChevronDown,FaEnvelopeOpen} from 'react-icons/fa'
+import { FaCalendar, FaUsers, FaPhoneAlt, FaChevronDown, FaEnvelopeOpen } from 'react-icons/fa'
 import logo from '../../assets/schoollogo.jpg'
 
 
@@ -20,120 +20,120 @@ const Navbar = () => {
     <div className='navbar'>
       <div className="hovering-board">
         <div className="hovering-board-first">
-        <Link to='/'>
-        <p><FaUsers/></p>
-          <p>Personalized Counseling</p>
-        </Link>
+          <Link to='/'>
+            <p><FaUsers /></p>
+            <p>Personalized Counseling</p>
+          </Link>
         </div>
         <div className="hovering-board-second">
-        <Link to='/notice-board'>
-          <p>
-            <FaCalendar/>
-          </p>
-          <p>Notices</p>
-        </Link>
-        <a>
-        <p><FaPhoneAlt/></p>
-        <p>+977-9812345678</p>
-        </a>
-        <a>
-        <p><FaEnvelopeOpen /></p>
-        <p>email@gmail.com</p>
-        </a>
+          <Link to='/notice-board'>
+            <p>
+              <FaCalendar />
+            </p>
+            <p>Notices</p>
+          </Link>
+          <a>
+            <p><FaPhoneAlt /></p>
+            <p>+977-9812345678</p>
+          </a>
+          <a>
+            <p><FaEnvelopeOpen /></p>
+            <p>email@gmail.com</p>
+          </a>
         </div>
       </div>
       <div className="actual-navbar">
-      <div class="icon">
+        {/* <div class="icon">
           <span class="toggle">☰</span>
-        </div>
+        </div> */}
         <div className="logo">
           <Link to='/'>
-          <img src={logo} alt="logo" />
+            <img src={logo} alt="logo" />
           </Link>
         </div>
-        
+
         <div className="menu-items">
           <ul>
             <li>
               <Link to='/'>
-              <p>
-                Home
-              </p>
+                <p>
+                  Home
+                </p>
               </Link>
             </li>
             <li>
               <Link to='/about'>
-              <p>
-                About
+                <p>
+                  About
                 </p>
               </Link>
             </li>
             <li>
               <Link to='/'>
-              <div className='courses-menu'
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    > 
-      <p>Courses &nbsp;
-      <FaChevronDown/>
-      </p>
-      {showOptions && (
-        <div className='items-menu'>
-          <div>
-          <Link to='2-science'>
-          <p>+2 Science</p>
-          </Link>
-          </div>
-          <div><Link to='bca'><p>BCA</p></Link></div>
-          <div><Link to='bsc-csit'><p>BSC CSIT</p></Link></div>
-        </div>
-      )}
-    </div>
+                <div className='courses-menu'
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <p>Courses &nbsp;
+                    <FaChevronDown />
+                  </p>
+                  {showOptions && (
+                    <div className='items-menu'>
+                      <div>
+                        <Link to='2-science'>
+                          <p>+2 Science</p>
+                        </Link>
+                      </div>
+                      <div><Link to='bca'><p>BCA</p></Link></div>
+                      <div><Link to='bsc-csit'><p>BSC CSIT</p></Link></div>
+                    </div>
+                  )}
+                </div>
               </Link>
             </li>
             <li>
               <Link to='/our-team'>
-              <p>Our Team</p>
+                <p>Our Team</p>
               </Link>
             </li>
             <li>
               <Link to='/contact'>
-              <p>Contact</p>
+                <p>Contact</p>
               </Link>
             </li>
           </ul>
         </div>
         <div className="form">
           <div className='admission-form'>
-          <Link to='/login/teacher'>
-          <p>Teacher Login</p>
-          </Link>
+            <Link to='/login/teacher'>
+              <p>Teacher Login</p>
+            </Link>
           </div>
           <div className='admission-form'>
-          <Link to='/login/student'>
-          <p>Student Login</p>
-          </Link>
+            <Link to='/login/student'>
+              <p>Student Login</p>
+            </Link>
           </div>
         </div>
       </div>
     </div>
-    
-    
-  
+
+
+
   )
-  $('.toggle').click(function () {
-    "use strict";
-    $('.navbar ul').slideToggle();
-});
+  //   $('.toggle').click(function () {
+  //     "use strict";
+  //     $('.navbar ul').slideToggle();
+  // });
 
 
 
-$(window).resize(function () {
-    "use strict";
-    if ($(window).width() > 780) {
-        $('.navbar ul').removeAttr('style');
-    }
-});
+  // $(window).resize(function () {
+  //     "use strict";
+  //     if ($(window).width() > 780) {
+  //         $('.navbar ul').removeAttr('style');
+  //     }
+  // });
 }
 
 export default Navbar
